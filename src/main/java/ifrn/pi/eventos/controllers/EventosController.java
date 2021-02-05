c33083d0cb0755871df21d4a216025782e1a8e06
NatanController <natangab007@gmail.com>
2021-02-05 02:06:05 -0300

    Criando a listagem de eventos

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,24 +1,29 @@
 package ifrn.pi.eventos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import ifrn.pi.eventos.models.Evento;
 import ifrn.pi.eventos.repositories.EventoRepository;
 
 
 @Controller
+@RequestMapping("/eventos")
 public class EventosController {
 	
 	@Autowired
 	private EventoRepository er;
 
-	@RequestMapping("/eventos/form")
+	@GetMapping("/form")
 	public String form() {
 		return "eventos/formEvento";
 };    
-    @PostMapping("/eventos/form")
+    @PostMapping
     public String envioForm(Evento evento) {
     	System.out.println(evento);
     	er.save(evento);
@@ -26,4 +31,16 @@ public class EventosController {
     	return "evento/formEnviado";
     	};
 
-}
+      @GetMapping
+      public ModelAndView listar() {
+	  
+	  List<Evento> eventos = er.findAll();
+	  ModelAndView mv = new ModelAndView ("eventos/lista");
+	  mv.addObject("eventos", eventos);
+      return mv;
+      };
+      
+      
+      
+      
+};
